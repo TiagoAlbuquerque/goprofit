a45392bd3e368bebbc4a96aef2e37336b52f7908
TiagoAlbuquerque <tiago.lbqrq@gmail.com>
2018-06-09 22:34:14 -0300

    mais

--- src/locations/locations.go
+++ src/locations/locations.go
@@ -1,41 +1,47 @@
 package locations
 
 import (
-    "fmt"
-    "../utils"
+	"fmt"
+
+	"../utils"
 )
 
-const f_name = "data_locations.eve"
-const locationsUrl = "https://esi.evetech.net/latest/universe/stations/%s"
-const structuresUrl = "https://stop.hammerti.me.uk/api/citadel/%s"
+const fName = "data_locations.eve"
+const locationsURL = "https://esi.evetech.net/latest/universe/stations/%s"
+const structuresURL = "https://stop.hammerti.me.uk/api/citadel/%s"
+
 var locations map[string]interface{}
-var saveToFileFlag bool = false
+var saveToFileFlag = false
 
 func getLocationInfo(id string) map[string]interface{} {
-    var url string
-    if len(id) == 8 { url = fmt.Sprintf(locationsUrl, id)
-    } else { url = fmt.Sprintf(structuresUrl, id) }
-    fmt.Println(url)
-    var location map[string]interface{}
-    utils.JsonFromUrl(url, &location)
-    cleanLocation(location)
-    locations[id] = location
-    saveToFileFlag = true
-    return location
+	var url string
+	if len(id) == 8 {
+		url = fmt.Sprintf(locationsURL, id)
+	} else {
+		url = fmt.Sprintf(structuresURL, id)
+	}
+	fmt.Println(url)
+	var location map[string]interface{}
+	utils.JsonFromUrl(url, &location)
+	locations[id] = location
+	saveToFileFlag = true
+	return location
 }
 
-func init(){
-    i_locations, err := utils.Load(f_name)
-    if err == nil {
-        locations = i_locations.(map[string]interface{})
-    } else {
-        fmt.Printf("Failed to open %s\n", f_name)
-        locations = make(map[string]interface{})
-    }
-    cleanup()
+func init() {
+	iLocations, err := utils.Load(fName)
+	if err == nil {
+		locations = iLocations.(map[string]interface{})
+	} else {
+		fmt.Printf("Failed to open %s\n", fName)
+		locations = make(map[string]interface{})
+	}
 }
 
+// Terminate locations
 func Terminate() {
-    if !saveToFileFlag { return }
-    utils.Save(f_name, locations)
+	if !saveToFileFlag {
+		return
+	}
+	utils.Save(fName, locations)
 }
