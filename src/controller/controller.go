0d850b4591f9d4c54c16f83d402007291d1f75cb
TiagoAlbuquerque <tiago.lbqrq@gmail.com>
2018-06-10 16:25:20 -0300

    ok

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -1,81 +1,82 @@
 package controller
 
 import (
-    "../deals"
-    "../items"
-    "../order"
-    "../regions"
-    "../shoppingLists"
-    "../utils"
-//    "../utils/avl"
+	"../deals"
+	"../items"
+	"../locations"
+	"../order"
+	"../regions"
+	"../shoppingLists"
+	"../utils"
+	//    "../utils/avl"
 
-    "fmt"
-    "encoding/json"
-    "io/ioutil"
-    "github.com/ti/nasync"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/ti/nasync"
 )
-
 
 func placeOrders(orders []order.Order) {
 
-    utils.StatusIndicator("Processing market page")
+	utils.StatusIndicator("Processing market page")
 
-    cDeals := make(chan deals.Deal)
-    defer close(cDeals)
-    go shoppingLists.ConsumeDeals(cDeals)
+	cDeals := make(chan deals.Deal)
+	defer close(cDeals)
+	go shoppingLists.ConsumeDeals(cDeals)
 
-    for _, o := range orders {
-        items.PlaceOrder(&o)
-        deals.ComputeDeals(&o, cDeals)
-    }
+	for _, o := range orders {
+		items.PlaceOrder(&o)
+		deals.ComputeDeals(&o, cDeals)
+	}
 }
 
 func consumePages(cPages chan []order.Order, cOK chan bool, total int) {
-    items.Cleanup()
-    for i := 0; i < total; i++ {
-        utils.StatusIndicator("Waiting page download")
-        placeOrders(<-cPages)
-        cOK <- true
-    }
+	items.Cleanup()
+	for i := 0; i < total; i++ {
+		utils.StatusIndicator("Waiting page download")
+		placeOrders(<-cPages)
+		cOK <- true
+	}
 }
 
-func getMarketPages(url string, cPages chan []order.Order){
-    var mPage []order.Order
-    for ok := false; !ok; {
-        res := utils.GetUrl(url)
-        defer res.Body.Close()
-        body, _ := ioutil.ReadAll(res.Body)
-        json.Unmarshal(body, &mPage)
-        ok = (mPage != nil)
-    }
-    cPages <- mPage
+func getMarketPages(url string, cPages chan []order.Order) {
+	var mPage []order.Order
+	for ok := false; !ok; {
+		res := utils.GetUrl(url)
+		defer res.Body.Close()
+		body, _ := ioutil.ReadAll(res.Body)
+		json.Unmarshal(body, &mPage)
+		ok = (mPage != nil)
+	}
+	cPages <- mPage
 }
 
-func FetchMarket(){
-    fmt.Println("Fetching markets pages")
-    lURL := regions.GetMarketsPagesList()
-    cOK := make(chan bool)
-    cPages := make(chan []order.Order)
-    total := len(lURL)
-    go consumePages(cPages, cOK, total)
-    async := nasync.New(1000, 1000)
-    defer async.Close()
-    for i := 0; i < total; i++ {
-        async.Do(getMarketPages, lURL[i], cPages)
-    }
-    utils.ProgressBar(total, cOK)
+func FetchMarket() {
+	fmt.Println("Fetching markets pages")
+	lURL := regions.GetMarketsPagesList()
+	cOK := make(chan bool)
+	cPages := make(chan []order.Order)
+	total := len(lURL)
+	go consumePages(cPages, cOK, total)
+	async := nasync.New(1000, 1000)
+	defer async.Close()
+	for i := 0; i < total; i++ {
+		async.Do(getMarketPages, lURL[i], cPages)
+	}
+	utils.ProgressBar(total, cOK)
 }
 
 func PrintShoppingLists(n int) {
-    shoppingLists.PrintTop(n)
+	shoppingLists.PrintTop(n)
 }
 
 func Terminate() {
-    items.Cleanup()
-    deals.Cleanup()
-    shoppingLists.Cleanup()
+	items.Cleanup()
+	deals.Cleanup()
+	shoppingLists.Cleanup()
 
-    items.Terminate()
-    regions.Terminate()
-
+	items.Terminate()
+	regions.Terminate()
+	locations.Terminate()
 }
